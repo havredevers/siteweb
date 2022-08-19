dbde67781f700503de5c5a20106f5e4c7f4fdaa6
Paidge <paidge_cs@hotmail.com>
2022-08-19 21:25:38 +0200

    correcting bugs in carousel

--- components/AccueilCarousel.vue
+++ components/AccueilCarousel.vue
@@ -55,7 +55,7 @@
     </div>
     <div class="pagination">
       <div class="prev">
-        <a title="Précédent" href="#" @click="plusSlides(-1)">
+        <a title="Précédent" href="#" @click="prev($event)">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
             <path
               d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"
@@ -64,7 +64,7 @@
         </a>
       </div>
       <div class="next">
-        <a title="Suivant" href="#" @click="plusSlides(1)">
+        <a title="Suivant" href="#" @click="next($event)">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
             <path
               d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"
@@ -91,31 +91,34 @@ export default {
     }
   },
   mounted() {
-    this.showSlides(this.slideIndex)
-    this.slideIndex++
     this.launch()
   },
   methods: {
     launch(e) {
       if (e) e.preventDefault()
-
       this.isPlaying = true
+      this.showSlides(this.slideIndex)
+
       this.interval = setInterval(
         function () {
-          this.showSlides(this.slideIndex)
-          this.slideIndex++
+          this.next()
         }.bind(this),
         5000
       )
     },
     pause(e) {
       e.preventDefault()
-
       clearInterval(this.interval)
       this.isPlaying = false
     },
-    plusSlides(n) {
-      this.showSlides((this.slideIndex += n))
+    prev(e) {
+      e.preventDefault()
+      this.showSlides((this.slideIndex = this.slideIndex - 1))
+    },
+    next(e) {
+      if (e) e.preventDefault()
+
+      this.showSlides((this.slideIndex = this.slideIndex + 1))
     },
     currentSlide(n) {
       this.showSlides((this.slideIndex = n))
