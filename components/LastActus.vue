722eedd0ae613935b943360057e1928fc1070753
paidge <paidge_cs@hotmail.com>
2022-09-27 15:52:17 +0200

    svg waves inline for improvement

--- components/LastActus.vue
+++ components/LastActus.vue
@@ -19,7 +19,22 @@
       </div>
     </div>
     <div class="wave">
-      <img src="/ui/wave_final2.svg" />
+      <svg
+        viewBox="0 0 1440 130"
+        version="1.1"
+        width="1440"
+        height="130"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          style="fill: #ead0a3; fill-opacity: 1"
+          d="M 475 0 L 475 0 L 80 0 L 0 0 L 0 63.349609 L 80 77.404297 C 159.99992 91.723908 320.00016 119.44071 475 119.63867 C 475 119.63865 475 119.6387 475 119.63867 L 475 0 z "
+        />
+        <path
+          style="fill: #f7e9d4; fill-opacity: 1"
+          d="M 475 0 L 475 119.63867 C 640.0389 119.43402 800.01969 91.723804 960 87.962891 C 1119.9998 84.465474 1280.0001 105.5824 1360 116.14062 L 1440 126.69922 L 1440 0 L 1360 0 L 960 0 L 475 0 z "
+        />
+      </svg>
     </div>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,22 @@
         </div>
       </div>
       <div class="wave">
-        <img src="/ui/wave_final.svg" />
+        <svg
+          viewBox="0 0 1440 130"
+          version="1.1"
+          width="1440"
+          height="130"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            style="fill: #e3ad89; fill-opacity: 1"
+            d="M 475 0 L 475 0 L 80 0 L 0 0 L 0 63.349609 L 80 77.404297 C 159.99992 91.723908 320.00016 119.44071 475 119.63867 C 475 119.63865 475 119.6387 475 119.63867 L 475 0 z "
+          />
+          <path
+            style="fill: #f4dbc9; fill-opacity: 1"
+            d="M 475 0 L 475 119.63867 C 640.0389 119.43402 800.01969 91.723804 960 87.962891 C 1119.9998 84.465474 1280.0001 105.5824 1360 116.14062 L 1440 126.69922 L 1440 0 L 1360 0 L 960 0 L 475 0 z "
+          />
+        </svg>
       </div>
     </section>
     <LastActus :articles="articles" />
@@ -108,8 +123,9 @@ export default {
     width: 100%;
     z-index: 2;
 
-    img {
+    svg {
       width: 100%;
+      height: auto;
     }
 
     @media (min-width: 850px) {
