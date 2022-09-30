24d5243aaa6a88d8c4ebde8a9ed43404a9f5374c
paidge <paidge_cs@hotmail.com>
2022-09-30 10:37:08 +0200

    init boutique

--- pages/boutique.vue
+++ pages/boutique.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <h2>La boutique</h2>
+  <div class="container">
+    <h1>La boutique</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto non
+      natus rem illum illo. Vitae quae, id facilis, ducimus doloribus sit maxime
+      cum odio aspernatur quo repellat nesciunt voluptatibus iusto!
+    </p>
   </div>
 </template>
+
+<style lang="scss"></style>
