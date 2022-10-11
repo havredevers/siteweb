89e8a44086b4d3604ab4688dd2b6603680b9dcf8
paidge <paidge_cs@hotmail.com>
2022-10-11 05:03:25 +0200

    bug AOS again

--- plugins/aos.js
+++ plugins/aos.js
@@ -10,22 +10,22 @@ class AosPlugin {
   install(Vue) {
     Vue.mixin({
       mounted() {
+        if (!this.$vnode?.data.nuxtChild) return
+
         if (this.$route.name === 'prestations') {
           const divs = document.querySelectorAll('.nuxt-content > *')
           divs.forEach((div) => {
             div.setAttribute('data-aos', 'fade-up')
           })
         }
-        console.log('init')
-        setTimeout(() => {
-          import('~/node_modules/aos').then((AOS) => AOS.init(this.config))
-        }, 100)
-      },
-      updated() {
-        this.$nextTick(function () {
-          console.log('hard refresh')
-          AOS.refreshHard()
+
+        document.querySelectorAll('img').forEach((img) => {
+          img.addEventListener('load', () => {
+            AOS.refreshHard()
+          })
         })
+
+        import('~/node_modules/aos').then((AOS) => AOS.init(this.config))
       },
     })
   }
